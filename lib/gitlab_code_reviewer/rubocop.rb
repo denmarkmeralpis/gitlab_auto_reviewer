394377666f04e211489ad564441230920f2644c5
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2023-07-22 19:44:30 +0800

    Update cop parser

--- lib/gitlab_code_reviewer/rubocop.rb
+++ lib/gitlab_code_reviewer/rubocop.rb
@@ -5,10 +5,10 @@ require 'fileutils'
 
 module GitlabCodeReviewer
   class Rubocop
-    def initialize(file:, format: default_message_format)
+    def initialize(file:, format: 'COP_NAME: COP_MESSAGE')
       @file = File.read(file)
+      @format = format
       @reports = JSON.parse(@file)
-      @format = default_message_format
       @data = @reports['files'].select { |report| report['offenses'].size > 0 }
     end
 
@@ -38,19 +38,15 @@ module GitlabCodeReviewer
     #  * COP_NAME - lint name
     #  * COP_MESSAGE - error message
     #  * LINE_NUMBER - line number of the offense
-    def default_message_format
-      'COP_NAME: COP_MESSAGE'
-    end
-
     def draft_a_thread_message(offense_hash)
       cop_name = offense_hash['cop_name']
       cop_error = offense_hash['message']
       line_number = offense_hash['location']['line']
 
-      @format.gsub!(/COP_NAME/, cop_name)
-      @format.gsub!(/COP_MESSAGE/, cop_error)
-      @format.gsub!(/LINE_NUMBER/, line_number.to_s)
-      @format
+      message = @format.gsub(/COP_NAME/, cop_name)
+      message = message.gsub(/COP_MESSAGE/, cop_error)
+      message = message.gsub(/LINE_NUMBER/, line_number.to_s)
+      message
     end
 
     def fetch_latest_mr_version
