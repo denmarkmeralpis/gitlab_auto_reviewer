fa511087ffd342b8e77dcd81b5c142922995d2dc
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2023-07-22 15:22:00 +0800

    Remove default spec

--- spec/gitlab_code_reviewer_spec.rb
+++ spec/gitlab_code_reviewer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe GitlabCodeReviewer do
   it "has a version number" do
     expect(GitlabCodeReviewer::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
